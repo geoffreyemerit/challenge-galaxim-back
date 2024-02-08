dab122a76b653ff49d6f5a027f7f9c104974f157
Geoffrey Emerit <98878861+geoffreyemerit@users.noreply.github.com>
2024-02-08 09:13:46 +0100

    Update WebMvcConfig.java

--- src/main/java/galaxim/challenge/config/WebMvcConfig.java
+++ src/main/java/galaxim/challenge/config/WebMvcConfig.java
@@ -10,8 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://challenge.galaxim.com")
+                .allowedOrigins("http://localhost:4200", "https://challenge.galaxim.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
     }
 }
