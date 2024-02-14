4a6fc9acc7f694bd5ec9ce5ed9d11934c3ed8cf8
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2024-02-14 09:59:12 +0100

    commit to push and reload

--- src/main/java/galaxim/challenge/config/SecurityConfig.java
+++ src/main/java/galaxim/challenge/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
         log.debug("Setting up CORS configuration");
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "https://challenge.galaxim.com"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "https://challenge.galaxim.com", "https://challenge.weelodge.fr"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
